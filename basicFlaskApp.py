5334061a96a36517279f8e784053723f16fe32bd
MatissesProjects <MatissesProjects>
2019-08-02 16:37:15 -0700

    Adding int requirement for passed path vars

--- basicFlaskApp.py
+++ basicFlaskApp.py
@@ -3,15 +3,11 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
-@app.route('/<numOfClassOne>', methods=['GET'])
-@app.route('/<numOfClassOne>/<numOfClassTwo>', methods=['GET'])
-def redirect_to_index(numOfClassOne=4, numOfClassTwo=4):
-    return render_template('index_templates.html', numOfClassOne=numOfClassOne, numOfClassTwo=numOfClassTwo)
-
-@app.route('/basic_calc/<a>/<b>')
-def do_calc(a,b):
-    solution = float(a) + float(b)
-    return "{} + {} = {}".format(a,b,solution)
+@app.route('/<int:numOfClassOne>', methods=['GET'])
+@app.route('/<int:numOfClassOne>/<int:numOfClassTwo>', methods=['GET'])
+def redirect_to_index(numOfClassOne=4, numOfClassTwo=4, numOfClassThree=4):
+    return render_template('index_templates.html',
+                           numOfClassOne=numOfClassOne, numOfClassTwo=numOfClassTwo, numOfClassThree=numOfClassThree)
 
 if __name__ == '__main__':
     app.run()
