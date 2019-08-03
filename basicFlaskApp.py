4c42ce7513424df83ae128187704b83251a450db
MatissesProjects <MatissesProjects>
2019-08-02 18:15:20 -0700

    Moving parameters into request vars, easier to set defaults

--- basicFlaskApp.py
+++ basicFlaskApp.py
@@ -1,13 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET'])
-@app.route('/<int:numOfClassOne>', methods=['GET'])
-@app.route('/<int:numOfClassOne>/<int:numOfClassTwo>', methods=['GET'])
-def redirect_to_index(numOfClassOne=4, numOfClassTwo=4, numOfClassThree=4):
+def redirect_to_index():
     return render_template('index_templates.html',
-                           numOfClassOne=numOfClassOne, numOfClassTwo=numOfClassTwo, numOfClassThree=numOfClassThree)
+                           numOfClassOne=request.args.get('numOfClassOne', 4),
+                           numOfClassTwo=request.args.get('numOfClassTwo', 4),
+                           numOfClassThree=request.args.get('numOfClassThree', 4))
 
 if __name__ == '__main__':
     app.run()
